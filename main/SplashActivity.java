f1bfddf544831ad43f8bb23ebb2e95e73b8a4db9
sarvanidivakarla <153927061+sarvanidivakarla@users.noreply.github.com>
2024-10-31 22:20:38 +0530

    Create SplashActivity.java

---
+++ main/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.weatherapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.weatherapp.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler();
+    private Runnable runnable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        runnable = new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        handler.postDelayed(runnable, 3000);
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
+    }
+}
